24ec8cd974f839eb372a10a3e8b85bc055eeb622
xiaosatufu <xiaosatufu@163.com>
2021-09-02 17:37:36 +0800

    add homework

---
+++ hello/src/com/homework/HomeWork01.java
@@ -0,0 +1,116 @@
+package com.homework;
+
+public class HomeWork01 {
+    public static void main(String[] args) {
+//        Person xiaoming = new Person("小明", 10, "学生");
+//        Person xiaohuang = new Person("小黄", 18, "学生");
+//        Person xiaodong = new Person("小东", 12, "学生");
+//        System.out.println(xiaoming);
+//        System.out.println(xiaohuang);
+//        System.out.println(xiaodong);
+//
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr.length-1-i; j++) {
+//                if(arr[j]>arr[j+1]) {
+//                    int temp = arr[i];
+//                    arr[j] = arr[j+i];
+//                    arr[j+i] = temp;
+//                }
+//            }
+//        }
+//
+        Person[] persons = new Person[3];
+        persons[0] = new Person("jack",10,"javaEE");
+        persons[1] = new Person("tom",50,"大数据");
+        persons[2] = new Person("mary",50,"php");
+        for (int i = 0; i < persons.length; i++) {
+            System.out.println(persons[i]);
+        }
+
+
+    }
+}
+
+class Person {
+    private String name;
+    private int age;
+    private String job;
+
+    public Person(String name, int age, String job) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", job='" + job + '\'' +
+                '}';
+    }
+}
+
+
+
+//class Person {
+//    String name;
+//    int age;
+//    String job;
+//
+//    public Person(String name, int age, String job) {
+//        this.name = name;
+//        this.age = age;
+//        this.job = job;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public String getJob() {
+//        return job;
+//    }
+//
+//    public void setJob(String job) {
+//        this.job = job;
+//    }
+//}
---
+++ hello/src/com/homework/HomeWork02.java
@@ -0,0 +1,14 @@
+package com.homework;
+
+public class HomeWork02 {
+    /**
+     * public 公共
+     * privite 私有权限 仅在类中可以调用
+     * protect 保护
+     *
+     *
+     *
+     *
+     *
+     */
+}
