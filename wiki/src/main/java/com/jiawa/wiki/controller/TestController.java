ad5fbee3fab5ad79c52cc786b46079d62c5b7cbe
xiaosatufu <xiaosatufu@163.com>
2021-09-13 16:26:51 +0800

    添加配置项

--- wiki/src/main/java/com/jiawa/wiki/controller/TestController.java
+++ wiki/src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,12 +1,18 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
+
     @RequestMapping("/hello")
     public String hello(){
-        return "hello";
+        return "hello"+testHello;
     }
 }
