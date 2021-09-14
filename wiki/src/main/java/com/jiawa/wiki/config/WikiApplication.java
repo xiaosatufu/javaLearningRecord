424bd2d6e8aa8f47f83f8675f45f7b6183f850b3
xiaosatufu <xiaosatufu@163.com>
2021-09-14 16:11:50 +0800

    mybatis使用示例

--- wiki/src/main/java/com/jiawa/wiki/config/WikiApplication.java
+++ wiki/src/main/java/com/jiawa/wiki/config/WikiApplication.java
@@ -1,11 +1,13 @@
 package com.jiawa.wiki.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("com.jiawa")
 @SpringBootApplication
+@MapperScan("com.jiawa.wiki.mapper")
 public class WikiApplication {
 
     public static void main(String[] args) {
--- wiki/src/main/java/com/jiawa/wiki/controller/TestController.java
+++ wiki/src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,8 +1,14 @@
 package com.jiawa.wiki.controller;
 
+import com.jiawa.wiki.domain.Test;
+import com.jiawa.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -10,9 +16,16 @@ public class TestController {
     @Value("${test.hello:TEST}")
     private String testHello;
 
+    @Resource
+    private TestService testService;
+
+
 
     @RequestMapping("/hello")
     public String hello(){
         return "hello"+testHello;
     }
+
+    @GetMapping("/test/list")
+    public List<Test> list() {return testService.list();}
 }
