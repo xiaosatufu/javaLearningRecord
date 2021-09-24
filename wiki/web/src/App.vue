6d8d49759dabeeb8a3a2f67e7fd7eefb6f0c9433
xiaosatufu <xiaosatufu@163.com>
2021-09-24 10:09:27 +0800

    只做the-header组件

--- wiki/web/src/App.vue
+++ wiki/web/src/App.vue
@@ -1,30 +1,22 @@
 <template>
   <a-layout id="components-layout-demo-top-side-2">
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          theme="dark"
-          mode="horizontal"
-          v-model:selectedKeys="selectedKeys1"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+      <the-header></the-header>
       <router-view></router-view>
-
-    <a-layout-footer style="text-align: center">
-      Ant Design ©2018 Created by Ant UED
-    </a-layout-footer>
+      <the-footer></the-footer>
   </a-layout>
 </template>
 <script lang="ts">
 // import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue';
 import { defineComponent, ref } from 'vue';
+
+import theHeader from '@/components/the-header'
+import theFooter from '@/components/the-footer'
+
 export default defineComponent({
+  name:'app',
   components: {
+    theHeader,
+    theFooter
     // UserOutlined,
     // LaptopOutlined,
     // NotificationOutlined,
